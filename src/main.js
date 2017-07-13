80a422e314a95dc2db4a182a8c7988e75f5a65d6
doganozturk <doganozturk2005@gmail.com>
2017-07-13 16:25:23 +0300

    vuex configuration.

--- src/main.js
+++ src/main.js
@@ -6,10 +6,14 @@ import App from "./components/App";
 // import router
 import router from "./router";
 
+// import Vuex store
+import store from "./store";
+
 // mount Vue
 // eslint-disable-next-line
 new Vue({
   el: "#app",
   router,
+  store,
   render: h => h(App)
 });
