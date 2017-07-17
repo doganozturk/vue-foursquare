438b35ce93d675a2d5e43149290055441e0cf0de
doganozturk <doganozturk2005@gmail.com>
2017-07-17 23:03:18 +0300

    tips container and all tips btn improvements.

--- src/views/components/Tips/Tips.vue.js
+++ src/views/components/Tips/Tips.vue.js
@@ -1,15 +1,27 @@
 export default {
   name: "Tips",
+  data() {
+    return {
+      allTips: false,
+      showAllTipsBtn: false
+    };
+  },
   props: {
     tips: {
       type: Array
     }
+  },
+  created() {
+    if (this.tips.length > 5) this.showAllTipsBtn = true;
   },
   methods: {
     getUserPhoto(photo) {
       const imgSize = "100x100";
 
       return `${photo.prefix}${imgSize}${photo.suffix}`;
+    },
+    toggleAllTips() {
+      this.allTips = !this.allTips;
     }
   }
 };
